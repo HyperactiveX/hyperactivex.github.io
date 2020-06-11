1b1be000ccf37d4d78aafffd9860ff1e0d75a1dd
Chalanthorn <s.chalanthorn@gmail.com>
2020-06-11 12:18:17 +0700

    [UPDATE] Update the features for mobile users

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "300px";
-  document.getElementById("main").style.marginLeft = "300px";
+  document.getElementById("mySidenav").style.width = "50vw";
+  document.getElementById("main").style.marginLeft = "50vw";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
@@ -11,3 +11,17 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
 }
+
+
+// function myFunction(x) {
+//   if (x.matches) { // If media query matches
+//       document.body.style.width = '360px';
+//   } else {
+//       document.body.style.width = 'auto';
+//   }
+// }
+
+// var x = window.matchMedia("(max-width: 360px)")
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes
+
