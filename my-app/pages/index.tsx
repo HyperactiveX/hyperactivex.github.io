23f1cd3103927ccee2bf7960854a00e3eb90b2d7
HyperactiveX <s.chalanthorn@gmail.com>
2021-07-29 12:11:43 +0700

    [UPDATE] : Init Nextjs project with typescript

---
+++ my-app/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tortoei | Homepage</title>
+        <meta name="description" content="Welcome to my world!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}></main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
