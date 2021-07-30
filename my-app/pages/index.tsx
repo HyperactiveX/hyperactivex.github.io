9ba36df1e1a823dd2f8c551aaf43f2158211e6d2
HyperactiveX <s.chalanthorn@gmail.com>
2021-07-30 13:25:59 +0700

    [UPDATE] : Change styling of the whole website

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -8,15 +8,31 @@ export default function Home() {
       <Layout title="Homepage | Tortoei">
         <div className={styles.container}>
           <div className={styles.boxContainer1}>
-            <div className={styles.description}>
-              <h1>Hello, I'm Toei</h1>
+            <div className={styles.innerBoxContainer}>
+              <div className={styles.profilePicture}>
+                <Image
+                  className={styles.image}
+                  alt="Picture of the author"
+                  src="/images/mypf.jpg"
+                  height={256}
+                  width={256}
+                />
+                <div className={styles.verticalLine} />
+              </div>
+              <div className={styles.description}>
+                <h1>Hi there, I'm Toei</h1>
+                <p>
+                  I'm a junior year student in Computer Science at King
+                  Mongkut's University of Technology Thonburi. I'm aspired to
+                  become a full stack developer.
+                </p>
+              </div>
             </div>
-            <div className={styles.profilePicture}>
-              {/* <Image
-                src="/images/mypf.jpg"
-                height="200px"
-                width="200px"
-              ></Image> */}
+            <div className={styles.statusContainer}>
+              <p className={styles.status}>Current progress: 57.4%</p>
+              <div className={styles.progressBar}>
+                <div className={styles.currentProgress} />
+              </div>
             </div>
           </div>
         </div>
