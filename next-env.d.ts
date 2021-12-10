415ec9ffe3019ec146efe0ff1ec39aec2f29efc3
HyperactiveX <s.chalanthorn@gmail.com>
2021-12-10 17:09:23 +0700

    [ADD] : Export CSS in build in package.json

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,6 +2,5 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-declare module '*.css';
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
