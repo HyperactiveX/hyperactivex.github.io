bca29380fc1c109e886da69ca09d4e154b1240ad
HyperactiveX <s.chalanthorn@gmail.com>
2021-12-28 14:14:17 +0700

    [UPDATE] : About me section

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -3,7 +3,16 @@ import styles from "../styles/pages/AboutMe.module.css"
 const AboutMe = () => {
   return (
     <div className={styles.content}>
-        <h2>THIS IS ABOUT ME SECTION</h2>
+        <div className={styles.imageBox}>
+          <img
+            className={styles.profileImage}
+            src={`/images/mypf.jpg`}
+            alt={`My profile picture`}
+            />
+        </div>
+        <div className={styles.descriptionBox}>
+          This contains description
+        </div>
     </div>
   );
 };
